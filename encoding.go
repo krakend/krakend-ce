66d041a061a750df287cafb539a3eab635aedbb3
kpacha <kpacha666@gmail.com>
2018-04-20 20:53:17 +0200

    remove encoding modules until fixed

--- encoding.go
+++ encoding.go
@@ -1,12 +1,12 @@
 package main
 
-import (
-	"github.com/devopsfaith/krakend-rss"
-	"github.com/devopsfaith/krakend-xml"
-)
+// import (
+// "github.com/devopsfaith/krakend-rss"
+// "github.com/devopsfaith/krakend-xml"
+// )
 
 // RegisterEncoders registers all the available encoders
 func RegisterEncoders() {
-	xml.Register()
-	rss.Register()
+	// xml.Register()
+	// rss.Register()
 }

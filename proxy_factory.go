fb0381437ad4cc77d40e6ef12fa259ec5a3a1582
kpacha <kpacha666@gmail.com>
2018-11-09 14:54:03 +0100

    json schema module added

--- proxy_factory.go
+++ proxy_factory.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	jsonschema "github.com/devopsfaith/krakend-jsonschema"
 	metrics "github.com/devopsfaith/krakend-metrics/gin"
 	opencensus "github.com/devopsfaith/krakend-opencensus"
 	"github.com/devopsfaith/krakend/logging"
@@ -10,6 +11,7 @@ import (
 // NewProxyFactory returns a new ProxyFactory wrapping the injected BackendFactory with the default proxy stack and a metrics collector
 func NewProxyFactory(logger logging.Logger, backendFactory proxy.BackendFactory, metricCollector *metrics.Metrics) proxy.Factory {
 	proxyFactory := proxy.NewDefaultFactory(backendFactory, logger)
+	proxyFactory = jsonschema.ProxyFactory(proxyFactory)
 	proxyFactory = metricCollector.ProxyFactory("pipe", proxyFactory)
 	proxyFactory = opencensus.ProxyFactory(proxyFactory)
 	return proxyFactory

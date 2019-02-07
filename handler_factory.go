069cb92869c9398a695c489cea02d6814a2417df
kpacha <kpacha666@gmail.com>
2019-02-07 15:21:56 +0100

    jose component upgraded to 0.8.0

--- handler_factory.go
+++ handler_factory.go
@@ -1,7 +1,8 @@
 package krakend
 
 import (
-	jose "github.com/devopsfaith/krakend-jose"
+	"github.com/devopsfaith/krakend-jose"
+	ginjose "github.com/devopsfaith/krakend-jose/gin"
 	metrics "github.com/devopsfaith/krakend-metrics/gin"
 	opencensus "github.com/devopsfaith/krakend-opencensus/router/gin"
 	juju "github.com/devopsfaith/krakend-ratelimit/juju/router/gin"
@@ -12,7 +13,7 @@ import (
 // NewHandlerFactory returns a HandlerFactory with a rate-limit and a metrics collector middleware injected
 func NewHandlerFactory(logger logging.Logger, metricCollector *metrics.Metrics, rejecter jose.Rejecter) router.HandlerFactory {
 	handlerFactory := juju.HandlerFactory
-	handlerFactory = jose.HandlerFactory(handlerFactory, logger, rejecter)
+	handlerFactory = ginjose.HandlerFactory(handlerFactory, logger, rejecter)
 	handlerFactory = metricCollector.NewHTTPHandlerFactory(handlerFactory)
 	handlerFactory = opencensus.New(handlerFactory)
 	return handlerFactory

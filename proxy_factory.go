7e3a8db2837e2f02a1c041924840146cac9f7b9d
kpacha <kpacha666@gmail.com>
2019-04-03 14:29:28 +0200

    shadow proxy factory added to the proxy factory stack

--- proxy_factory.go
+++ proxy_factory.go
@@ -12,6 +12,7 @@ import (
 // NewProxyFactory returns a new ProxyFactory wrapping the injected BackendFactory with the default proxy stack and a metrics collector
 func NewProxyFactory(logger logging.Logger, backendFactory proxy.BackendFactory, metricCollector *metrics.Metrics) proxy.Factory {
 	proxyFactory := proxy.NewDefaultFactory(backendFactory, logger)
+	proxyFactory = proxy.NewShadowFactory(proxyFactory)
 	proxyFactory = jsonschema.ProxyFactory(proxyFactory)
 	proxyFactory = cel.ProxyFactory(logger, proxyFactory)
 	proxyFactory = metricCollector.ProxyFactory("pipe", proxyFactory)

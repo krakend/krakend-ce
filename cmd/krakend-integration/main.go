3a7cdc83c6fee8cbbbee6183c7647fef817a1a19
kpacha <kpacha666@gmail.com>
2021-01-18 17:40:23 +0100

    better error reporting added to the integration test runner

--- cmd/krakend-integration/main.go
+++ cmd/krakend-integration/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 
 	"github.com/devopsfaith/krakend-ce/tests"
 )
@@ -13,16 +14,27 @@ func main() {
 	runner, tcs, err := tests.NewIntegration(nil, nil, nil)
 	if err != nil {
 		fmt.Println(err)
+		os.Exit(1)
 		return
 	}
 	defer runner.Close()
 
+	errors := 0
+
 	for _, tc := range tcs {
 		if err := runner.Check(tc); err != nil {
+			errors++
 			fmt.Printf("%s: %s\n", tc.Name, err.Error())
-			return
+			continue
 		}
 		fmt.Printf("%s: ok\n", tc.Name)
 	}
 	fmt.Printf("%d test completed\n", len(tcs))
+
+	if errors == 0 {
+		return
+	}
+
+	fmt.Printf("%d test failed\n", errors)
+	os.Exit(1)
 }

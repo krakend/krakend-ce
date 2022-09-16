d8cc196cda4ee5f0362c3bc56c5a1da45b3c2d93
Daniel Ortiz <dortiz@krakend.io>
2022-09-16 17:11:48 +0200

    Be able to configure through the router extra config the NoRoute and NoMethod handlers to return a custom json as body.

--- router_engine.go
+++ router_engine.go
@@ -1,6 +1,8 @@
 package krakend
 
 import (
+	"encoding/json"
+
 	"github.com/gin-gonic/gin"
 
 	botdetector "github.com/krakendio/krakend-botdetector/v2/gin"
@@ -19,6 +21,18 @@ func NewEngine(cfg config.ServiceConfig, opt luragin.EngineOptions) *gin.Engine 
 
 	engine.NoRoute(opencensus.HandlerFunc(&config.EndpointConfig{Endpoint: "NoRoute"}, defaultHandler, nil))
 	engine.NoMethod(opencensus.HandlerFunc(&config.EndpointConfig{Endpoint: "NoMethod"}, defaultHandler, nil))
+	if v, ok := cfg.ExtraConfig[luragin.Namespace]; ok && v != nil {
+		var ginOpts ginOptions
+		if b, err := json.Marshal(v); err == nil {
+			json.Unmarshal(b, &ginOpts)
+		}
+		if ginOpts.ErrorBody.Err404 != nil {
+			engine.NoRoute(opencensus.HandlerFunc(&config.EndpointConfig{Endpoint: "NoRoute"}, jsonHandler(404, ginOpts.ErrorBody.Err404), nil))
+		}
+		if ginOpts.ErrorBody.Err405 != nil {
+			engine.NoMethod(opencensus.HandlerFunc(&config.EndpointConfig{Endpoint: "NoMethod"}, jsonHandler(405, ginOpts.ErrorBody.Err405), nil))
+		}
+	}
 
 	logPrefix := "[SERVICE: Gin]"
 	if err := httpsecure.Register(cfg.ExtraConfig, engine); err != nil && err != httpsecure.ErrNoConfig {
@@ -39,8 +53,24 @@ func defaultHandler(c *gin.Context) {
 	c.Header(server.CompleteResponseHeaderName, server.HeaderIncompleteResponseValue)
 }
 
+func jsonHandler(status int, v interface{}) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		defaultHandler(c)
+		c.JSON(status, v)
+	}
+}
+
 type engineFactory struct{}
 
 func (engineFactory) NewEngine(cfg config.ServiceConfig, opt luragin.EngineOptions) *gin.Engine {
 	return NewEngine(cfg, opt)
+}
+
+type ginOptions struct {
+	// ErrorBody sets the json body to return to handlers like NoRoute (404) and NoMethod (405)
+	// Example: "404": { "error": "Not Found", "status": 404 }
+	ErrorBody struct {
+		Err404 interface{} `json:"404"`
+		Err405 interface{} `json:"405"`
+	} `json:"error_body"`
 }

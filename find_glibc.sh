42a5bfdfcd2d5ad2733889a34dac55a62c8b141a
Daniel Ortiz <dortiz@krakend.io>
2022-10-20 16:50:18 +0200

    Remove spaces from the glib variable.
    Signed-off-by: Daniel Ortiz <dortiz@krakend.io>

--- find_glibc.sh
+++ find_glibc.sh
@@ -5,17 +5,17 @@ GLIBC=UNKNOWN-0.0.0
 
 function get_os_version {
     source /etc/os-release
-    os_release="${NAME} ${VERSION_ID}"
+    os_release="${NAME}-${VERSION_ID}"
 }
 
 case $OS in
 Linux*)
     if ldd --version 2>&1 | grep -i musl > /dev/null; then
         get_os_version
-        GLIBC="MUSL-$(ldd --version 2>&1 | grep Version | cut -d\  -f2) ($os_release)"
+        GLIBC="MUSL-$(ldd --version 2>&1 | grep Version | cut -d\  -f2)_($os_release)"
     else
         get_os_version
-        GLIBC="GLIBC-$(ldd --version 2>&1  | grep ^ldd | awk '{print $(NF)}') ($os_release)"
+        GLIBC="GLIBC-$(ldd --version 2>&1  | grep ^ldd | awk '{print $(NF)}')_($os_release)"
     fi
     ;;
 Darwin*)
